62c028fab75f05604d66a21d190b67c81c0a86e7
Brian Simmons <bsimmons01@gmail.com>
2025-07-22 14:11:09 -0700

    Initial Commit

---
+++ iTunesSongSearch/iTunesSongSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesSongSearchApp.swift
+//  iTunesSongSearch
+//
+//  Created by Brian Simmons on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct iTunesSongSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
