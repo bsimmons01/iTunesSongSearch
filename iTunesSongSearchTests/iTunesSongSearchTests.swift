8e344c07dccd6721586784245fec6ab0336992ee
Brian Simmons <bsimmons01@gmail.com>
2025-07-22 14:56:58 -0700

    A few initial unit and UI tests which can be expanded upon.

--- iTunesSongSearchTests/iTunesSongSearchTests.swift
+++ iTunesSongSearchTests/iTunesSongSearchTests.swift
@@ -10,27 +10,57 @@ import XCTest
 
 final class iTunesSongSearchTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+  func testDecodeSongSearchResponse() throws {
+    let json = """
+    {
+      "resultCount": 1,
+      "results": [
+        {
+          "trackId": 123456,
+          "trackName": "Test Track",
+          "artistName": "Test Artist",
+          "collectionName": "Test Album",
+          "artworkUrl100": "https://example.com/art.jpg",
+          "previewUrl": "https://example.com/audio.m4a",
+          "releaseDate": "2022-01-01T00:00:00Z",
+          "primaryGenreName": "Test Genre"
         }
+      ]
     }
+    """.data(using: .utf8)!
 
+    let decoder = JSONDecoder()
+    decoder.dateDecodingStrategy = .iso8601
+
+    let response = try decoder.decode(SongSearchResponse.self, from: json)
+    XCTAssertEqual(response.resultCount, 1)
+
+    let song = response.results.first
+    XCTAssertEqual(song?.trackName, "Test Track")
+    XCTAssertEqual(song?.artistName, "Test Artist")
+    XCTAssertEqual(song?.primaryGenreName, "Test Genre")
+  }
+
+  func testEmptySearchResponse() throws {
+    let json = """
+  {
+    "resultCount": 0,
+    "results": []
+  }
+  """.data(using: .utf8)!
+    
+    let decoder = JSONDecoder()
+    let response = try decoder.decode(SongSearchResponse.self, from: json)
+
+    XCTAssertEqual(response.resultCount, 0)
+    XCTAssertTrue(response.results.isEmpty)
+  }
 }
